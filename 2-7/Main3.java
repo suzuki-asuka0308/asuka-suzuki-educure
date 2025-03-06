5a74825547917d7a2aea529d6996d409401a4ea7
suzuki-asuka0308 <suzuki.asuka0308@gmail.com>
2025-03-06 22:14:27 +0900

    Add files via upload

---
+++ 2-7/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int attackpower = 15;
+        int defensepower = 5;
+       // int damege = (attackpower *2 - defensepower)/3;
+
+        System.out.println("与えられるダメージ:" + (attackpower *2 - defensepower)/3);
+    }
+    
+}
